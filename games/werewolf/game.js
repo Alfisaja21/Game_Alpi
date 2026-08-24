dbb17ad790cc79ee847d9df2ca07558af813ddbd
Alfisaja21 <elfiyhidayat@gmail.com>
2026-08-24 23:49:37 +0700

    Delete games/werewolf directory

--- games/werewolf/game.js
+++
@@ -1,9 +0,0 @@
-
-function goBack(){
-window.location.href="../../index.html";
-}
-
-const join=document.querySelector('.join');
-const modal=document.getElementById('joinModal');
-
-join.onclick=()=>{modal.style.display='flex';};
